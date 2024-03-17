59d41571d93412c4d8382bb6361c15b7a7e566ac
ShinobiHere <tahabaloch0011@gmail.com>
2024-03-17 19:44:51 +0500

    firstcommit

---
+++ src/components/Veggie.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import { Link } from "react-router-dom";
+
+function Veggie() {
+  const [veggie, setVeggie] = useState([]);
+
+
+  useEffect(() => {
+    getVeggie();
+  }, []);
+
+  const getVeggie = async () => {
+    const check = localStorage.getItem("veggie");
+
+    if (check) {
+      setVeggie(JSON.parse(check));
+    } else {
+      const fetchData = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=5&tags=vegetarian`
+      );
+      const data = await fetchData.json();
+
+      localStorage.setItem("veggie", JSON.stringify(data.recipes));
+
+      setVeggie(data.recipes);
+    }
+  };
+
+  return (
+    <div>
+    <Wrapper>
+      <h3>Our Vegetarian Picks</h3>
+      <Splide
+        options={{
+          perPage: 2,
+          pagination: false,
+          drag: "free",
+          gap: "1rem",
+        }}
+      >
+        {veggie.map((recipe) => {
+          return (
+            <SplideSlide key={recipe.id}>
+              <Card>
+                <Link to={"/recipe/" + recipe.id}>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt={recipe.title} />
+              <Gradient/>
+                </Link>
+              </Card>
+            </SplideSlide>
+          );
+        })}
+      </Splide>
+    </Wrapper>
+    </div>
+  )
+}
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 20rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    left: 0;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 0%;
+    bottom: 5%;
+    padding: 10px 0 ;
+    transform: translate(0%, -50%);
+    color: #fff;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+  }
+`;
+
+const Gradient = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 3;
+  background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.5));`;
+
+export default Veggie
